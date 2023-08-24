2c9183d7dac7ffc4ccef78486da795ec8ad9bd4f
Mudacumura Brunoblaise <brunoblaise94@gmail.com>
2023-08-24 16:48:00 +0200

    cors

--- app.js
+++ app.js
@@ -6,16 +6,17 @@ const { errors } = require('celebrate');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJson = require('./docs/swagger.json');
 app.use(express.json());
-const corsOptions = {
-    origin:["http://localhost:3000/"]
-}
+//const corsOptions = {
+ //   origin:["http://localhost:3000/"]
+//}
 
 app.use('/api/v1', require('./src/api/v1/routers/index'));
 
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 //console.log('All models were synchronized successfully.');
 app.use(express.urlencoded({ extended: false }));
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
+app.use(cors());
 app.use(morgan('dev'));
 app.use(errors());
 
