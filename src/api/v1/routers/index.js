37e8623389bd7271f4163e4b7061a9ee285f96c0
Mudacumura Brunoblaise <brunoblaise94@gmail.com>
2023-08-25 12:12:21 +0200

    me

--- src/api/v1/routers/index.js
+++ src/api/v1/routers/index.js
@@ -1,11 +1,12 @@
 const user = require('./user.route');
 const pizza = require('./pizza.route');
 const order = require('./order.route');
+const me = require('../me.route')
 const express = require('express');
 const router = express.Router();
 
 router.use('/auth', user);
 router.use('/pizza', pizza);
 router.use('/order', order);
-
+router.use('/verify', me)
 module.exports = router;
