5e2da3e1abdc0cf28c874c0945a2addefee59480
Mudacumura Brunoblaise <brunoblaise94@gmail.com>
2023-08-29 10:34:28 +0200

    bulk

--- src/api/v1/controllers/order-create.controller.js
+++ src/api/v1/controllers/order-create.controller.js
@@ -12,12 +12,12 @@ module.exports = async function (req, res, next) {
 			return res.status(400).json({ msg: 'Please enter all fields', STATUS: 400, time: timeStamp() });
 		}
 
-		const order = await Order.create({
+		const order = await Order.bulkCreate([{
 			userId,
 			pizzaId,
 			quantity,
 			include: [Pizza, User],
-		});
+		}]);
 		res.status(201).json({ STATUS: 201, data: order, time: timeStamp() });
 	} catch (error) {
 		res.status(500).json({ msg: 'Server error', STATUS: 500, time: timeStamp() });
